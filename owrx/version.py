e5113c66740a23b868eeee220c7d13b88bdc638b
Marat Fayzullin <luarvique@gmail.com>
2023-05-06 23:30:26 -0400

    Incrementing version to 1.2.14.

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.13"
+_versionstring = "1.2.14"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
