9b087fd68a762f94ed5191154f2ed620ccbaf877
Marat Fayzullin <luarvique@gmail.com>
2023-05-19 18:59:54 -0400

    Showing timestamp as address for now.

--- htdocs/lib/MessagePanel.js
+++ htdocs/lib/MessagePanel.js
@@ -297,7 +297,7 @@ FlexMessagePanel.prototype.pushMessage = function(msg) {
     var $b = $(this.el).find('tbody');
     $b.append($(
         '<tr>' +
-            '<td class="address">' + msg.address + '</td>' +
+            '<td class="address">' + msg.timestamp + '</td>' +
             '<td class="message">' + msg.message + '</td>' +
         '</tr>'
     ));
