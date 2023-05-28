10847c46fd8fb448ef81fe58388d9d9d8efda86c
Marat Fayzullin <luarvique@gmail.com>
2023-05-28 16:26:26 -0400

    rtl_433 now shows signals from car keys and garage openers in ANALYZE mode.

--- csdr/module/rtl433.py
+++ csdr/module/rtl433.py
@@ -3,15 +3,16 @@ from csdr.module import PopenModule
 
 
 class Rtl433Module(PopenModule):
-    def __init__(self, sampleRate: int = 24000):
+    def __init__(self, sampleRate: int = 48000):
         self.sampleRate = sampleRate
         super().__init__()
 
     def getCommand(self):
         return [
-            "rtl_433", "-r", "cs16:-", "-f", "0", "-s", str(self.sampleRate),
+            "rtl_433", "-r", "cs16:-", "-F", "json", "-s", str(self.sampleRate),
             "-R", "-80",  "-R", "-149", "-R", "-154", "-R", "-160",
-            "-R", "-161", "-R", "-167", "-R", "-178",
+            "-R", "-161", "-R", "-167", "-R", "-178", "-R", "64",
+#            "-A",
         ]
 
     def getInputFormat(self) -> Format:
