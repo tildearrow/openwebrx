c23cb231a684f47e5d9bfd4e8f879552432b5294
Marat Fayzullin <luarvique@gmail.com>
2023-05-28 23:18:39 -0400

    Disabling decoders which need >48kHz.

--- csdr/module/rtl433.py
+++ csdr/module/rtl433.py
@@ -14,8 +14,10 @@ class Rtl433Module(PopenModule):
             "-F", "json" if self.jsonOutput else "kv",
 # These need 48kHz, 24kHz is not enough for them
 #            "-R", "-80",  "-R", "-149", "-R", "-154", "-R", "-160",
-#            "-R", "-161", "-R", "-167", "-R", "-178",
+#            "-R", "-161",
 #            "-R", "64",
+            # These need >48kHz bandwidth
+            "-167", "-R", "-178",
             "-A",
         ]
 
