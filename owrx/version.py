3a2265bbbd66283c2879ef76227008b30e8c1ec2
Marat Fayzullin <luarvique@gmail.com>
2023-07-06 11:52:24 -0400

    Preparing OWRX+ 1.2.22 release.

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.21"
+_versionstring = "1.2.22"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
