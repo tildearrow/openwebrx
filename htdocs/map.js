7e0239d9b5653ec786c956983dacedcca3442403
Marat Fayzullin <luarvique@gmail.com>
2023-07-30 12:18:50 -0400

    Separated locators from the map implementation.

--- htdocs/map.js
+++ htdocs/map.js
@@ -42,93 +42,28 @@ $(function(){
     var ws_url = href + "ws/";
 
     var map;
-    var rectangles = {};
     var receiverMarker;
     var updateQueue = [];
 
-    var strokeOpacity = 0.8;
-    var fillOpacity = 0.35;
-
-    // marker manager
+    // marker and locator managers
     var markmanager = null;
+    var locmanager = null;
 
     // clock
     var clock = new Clock($("#openwebrx-clock-utc"));
 
-    var colorKeys = {};
-    var colorScale = chroma.scale(['red', 'blue', 'green']).mode('hsl');
-    var getColor = function(id){
-        if (!id) return "#ffffff00";
-        if (!colorKeys[id]) {
-            var keys = Object.keys(colorKeys);
-            keys.push(id);
-            keys.sort(function(a, b) {
-                var pa = parseFloat(a);
-                var pb = parseFloat(b);
-                if (isNaN(pa) || isNaN(pb)) return a.localeCompare(b);
-                return pa - pb;
-            });
-            var colors = colorScale.colors(keys.length);
-            colorKeys = {};
-            keys.forEach(function(key, index) {
-                colorKeys[key] = colors[index];
-            });
-            reColor();
-            updateLegend();
-        }
-        return colorKeys[id];
-    }
-
-    // when the color palette changes, update all grid squares with new color
-    var reColor = function() {
-        $.each(rectangles, function(_, r) {
-            var color = getColor(colorAccessor(r));
-            r.setOptions({
-                strokeColor: color,
-                fillColor: color
-            });
-        });
-    }
-
-    var colorMode = 'byband';
-    var colorAccessor = function(r) {
-        switch (colorMode) {
-            case 'byband':
-                return r.band;
-            case 'bymode':
-                return r.mode;
-            case 'off':
-                return '';
-        }
-    };
-
-    $(function(){
-        $('#openwebrx-map-colormode').on('change', function(){
-            colorMode = $(this).val();
-            colorKeys = {};
-            filterRectangles(allRectangles);
-            reColor();
-            updateLegend();
+    $(function() {
+        $('#openwebrx-map-colormode').on('change', function() {
+            locmanager.setColorMode(map, $(this).val());
         });
     });
 
-    var updateLegend = function() {
-        var lis = $.map(colorKeys, function(value, key) {
-            // fake rectangle to test if the filter would match
-            var fakeRectangle = Object.fromEntries([[colorMode.slice(2), key]]);
-            var disabled = rectangleFilter(fakeRectangle) ? '' : ' disabled';
-            return '<li class="square' + disabled + '" data-selector="' + key + '"><span class="illustration" style="background-color:' + chroma(value).alpha(fillOpacity) + ';border-color:' + chroma(value).alpha(strokeOpacity) + ';"></span>' + key + '</li>';
-        });
-        $(".openwebrx-map-legend .content").html('<ul>' + lis.join('') + '</ul>');
-    }
-
     var processUpdates = function(updates) {
-        if ((typeof(AprsMarker) == 'undefined') || (typeof(FeatureMarker) == 'undefined')) {
+        if (!markmanager || !locmanager) {
             updateQueue = updateQueue.concat(updates);
             return;
         }
-        updates.forEach(function(update){
-
+        updates.forEach(function(update) {
             switch (update.location.type) {
                 case 'latlon':
                     var pos = new google.maps.LatLng(update.location.lat, update.location.lon);
@@ -173,6 +108,7 @@ $(function(){
                         showMarkerInfoWindow(infowindow.callsign, pos);
                     }
                 break;
+
                 case 'feature':
                     var pos = new google.maps.LatLng(update.location.lat, update.location.lon);
                     var marker = markmanager.find(update.callsign);
@@ -221,50 +157,40 @@ $(function(){
                         showMarkerInfoWindow(infowindow.callsign, pos);
                     }
                 break;
+
                 case 'locator':
-                    var loc = update.location.locator;
-                    var lat = (loc.charCodeAt(1) - 65 - 9) * 10 + Number(loc[3]);
-                    var lon = (loc.charCodeAt(0) - 65 - 9) * 20 + Number(loc[2]) * 2;
-                    var center = new google.maps.LatLng({lat: lat + .5, lng: lon + 1});
-                    var rectangle;
-                    // the accessor is designed to work on the rectangle... but it should work on the update object, too
-                    var color = getColor(colorAccessor(update));
-                    if (rectangles[update.callsign]) {
-                        rectangle = rectangles[update.callsign];
-                    } else {
-                        rectangle = new google.maps.Rectangle();
-                        rectangle.addListener('click', function(){
+                    var rectangle = locmanager.find(update.callsign);
+
+                    // If new item, create a new locator for it
+                    if (!rectangle) {
+                        rectangle = new GLocator();
+                        locmanager.add(update.callsign, rectangle);
+                        rectangle.addListener('click', function() {
                             showLocatorInfoWindow(this.locator, this.center);
                         });
-                        rectangles[update.callsign] = rectangle;
                     }
-                    rectangle.lastseen = update.lastseen;
-                    rectangle.locator = update.location.locator;
-                    rectangle.mode = update.mode;
-                    rectangle.band = update.band;
-                    rectangle.center = center;
 
-                    rectangle.setOptions($.extend({
-                        strokeColor: color,
-                        strokeWeight: 2,
-                        fillColor: color,
-                        map: rectangleFilter(rectangle) ? map : undefined,
-                        bounds:{
-                            north: lat,
-                            south: lat + 1,
-                            west: lon,
-                            east: lon + 2
-                        }
-                    }, getRectangleOpacityOptions(update.lastseen) ));
+                    // Update locator attributes, center, and age
+                    rectangle.age(new Date().getTime() - update.lastseen);
+                    rectangle.update(update);
+
+                    // Apply locator options
+                    var color = locmanager.getColor(rectangle);
+                    rectangle.setOptions({
+                        map : locmanager.filter(rectangle)? map : undefined,
+                        strokeColor  : color,
+                        strokeWeight : 2,
+                        fillColor    : color
+                    });
 
                     if (expectedLocator && expectedLocator == update.location.locator) {
-                        map.panTo(center);
-                        showLocatorInfoWindow(expectedLocator, center);
+                        map.panTo(rectangle.center);
+                        showLocatorInfoWindow(expectedLocator, rectangle.center);
                         expectedLocator = false;
                     }
 
                     if (infowindow && infowindow.locator && infowindow.locator == update.location.locator) {
-                        showLocatorInfoWindow(infowindow.locator, center);
+                        showLocatorInfoWindow(infowindow.locator, rectangle.center);
                     }
                 break;
             }
@@ -275,8 +201,7 @@ $(function(){
         var reset = function(callsign, item) { item.setMap(); };
         receiverMarker.setMap();
         markmanager.clear();
-        $.each(rectangles, reset);
-        rectangles = {};
+        locmanager.clear();
     };
 
     var reconnect_timeout = false;
@@ -321,8 +246,18 @@ $(function(){
 
                                 $.getScript('static/lib/MarkerManager.js').done(function(){
                                     markmanager = new MarkerManager();
-                                    processUpdates(updateQueue);
-                                    updateQueue = [];
+                                    if (locmanager) {
+                                        processUpdates(updateQueue);
+                                        updateQueue = [];
+                                    }
+                                });
+
+                                $.getScript('static/lib/LocatorManager.js').done(function(){
+                                    locmanager = new LocatorManager();
+                                    if (markmanager) {
+                                        processUpdates(updateQueue);
+                                        updateQueue = [];
+                                    }
                                 });
 
                                 var $legend = $(".openwebrx-map-legend");
@@ -341,6 +276,7 @@ $(function(){
                                     title: config['receiver_name'],
                                     config: config
                                 });
+
                             }); else {
                                 receiverMarker.setOptions({
                                     map: map,
@@ -424,28 +360,7 @@ $(function(){
     var showLocatorInfoWindow = function(locator, pos) {
         var infowindow = getInfoWindow();
         infowindow.locator = locator;
-        var inLocator = $.map(rectangles, function(r, callsign) {
-            return {callsign: callsign, locator: r.locator, lastseen: r.lastseen, mode: r.mode, band: r.band}
-        }).filter(rectangleFilter).filter(function(d) {
-            return d.locator == locator;
-        }).sort(function(a, b){
-            return b.lastseen - a.lastseen;
-        });
-        var distance = receiverMarker?
-            " at " + Marker.distanceKm(receiverMarker.position, pos) + " km" : "";
-        infowindow.setContent(
-            '<h3>Locator: ' + locator + distance + '</h3>' +
-            '<div>Active Callsigns:</div>' +
-            '<ul>' +
-                inLocator.map(function(i){
-                    var timestring = moment(i.lastseen).fromNow();
-                    var message = Marker.linkify(i.callsign) + ' (' + timestring + ' using ' + i.mode;
-                    if (i.band) message += ' on ' + i.band;
-                    message += ')';
-                    return '<li>' + message + '</li>'
-                }).join("") +
-            '</ul>'
-        );
+        infowindow.setContent(locmanager.getInfoHTML(locator, pos, receiverMarker));
         infowindow.setPosition(pos);
         infowindow.open(map);
     };
@@ -468,46 +383,11 @@ $(function(){
         infowindow.open(map, marker);
     }
 
-    var getScale = function(lastseen) {
-        var age = new Date().getTime() - lastseen;
-        var scale = 1;
-        if (age >= retention_time / 2) {
-            scale = (retention_time - age) / (retention_time / 2);
-        }
-        return Math.max(0, Math.min(1, scale));
-    };
-
-    var getRectangleOpacityOptions = function(lastseen) {
-        var scale = getScale(lastseen);
-        return {
-            strokeOpacity: strokeOpacity * scale,
-            fillOpacity: fillOpacity * scale
-        };
-    };
-
-    // fade out / remove positions after time
+    // Fade out / remove positions after time
     setInterval(function(){
-        var now = new Date().getTime();
-        $.each(rectangles, function(callsign, m) {
-            var age = now - m.lastseen;
-            if (age > retention_time) {
-                delete rectangles[callsign];
-                m.setMap();
-                return;
-            }
-            m.setOptions(getRectangleOpacityOptions(m.lastseen));
-        });
-        markmanager.ageAll();
+        if (locmanager)  locmanager.ageAll();
+        if (markmanager) markmanager.ageAll();
     }, 1000);
-
-    var rectangleFilter = allRectangles = function() { return true; };
-
-    var filterRectangles = function(filter) {
-        rectangleFilter = filter;
-        $.each(rectangles, function(_, r) {
-            r.setMap(rectangleFilter(r) ? map : undefined);
-        });
-    };
 
     var setupLegendFilters = function($legend) {
         $content = $legend.find('.content');
@@ -516,18 +396,13 @@ $(function(){
             $lis = $content.find('li');
             if ($lis.hasClass('disabled') && !$el.hasClass('disabled')) {
                 $lis.removeClass('disabled');
-                filterRectangles(allRectangles);
+                locmanager.setFilter(map);
             } else {
                 $el.removeClass('disabled');
                 $lis.filter(function() {
                     return this != $el[0]
                 }).addClass('disabled');
-
-                var key = colorMode.slice(2);
-                var selector = $el.data('selector');
-                filterRectangles(function(r) {
-                    return r[key] === selector;
-                });
+                locmanager.setFilter(map, $el.data('selector'));
             }
         });
 
