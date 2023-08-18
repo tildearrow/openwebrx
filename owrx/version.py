60994358c383f414d7aec86f714e0f1ca7787c5a
Marat Fayzullin <luarvique@gmail.com>
2023-08-18 00:26:25 -0400

    Preparing for 1.2.26 release.

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.25"
+_versionstring = "1.2.26"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
