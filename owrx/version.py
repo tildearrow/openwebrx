33150a05e992251a1fff3aaac3ed69323c072814
Marat Fayzullin <luarvique@gmail.com>
2023-09-06 23:05:30 -0400

    Updating version string.

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.28"
+_versionstring = "1.2.29"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
