54e9a3dd0b214efa6309fdbca067535f2355aa94
Marat Fayzullin <luarvique@gmail.com>
2023-09-11 12:20:07 -0400

    Painting YES/NO feature indicators red and green.

--- htdocs/features.js
+++ htdocs/features.js
@@ -1,5 +1,12 @@
 $(function(){
-    var converter = new showdown.Converter();
+    var converter = new showdown.Converter({openLinksInNewWindow: true});
+
+    function yes_no(v) {
+        return v?
+            '<td style="color:green;">YES</td>' :
+            '<td style="color:red;">NO</td>';
+    }
+
     $.ajax('api/features').done(function(data){
         var $table = $('table.features');
         $.each(data, function(name, details) {
@@ -8,13 +15,13 @@ $(function(){
                            '<td></td>' +
                            '<td>' + name + '</td>' +
                            '<td>' + converter.makeHtml(r.description) + '</td>' +
-                           '<td>' + (r.available ? 'YES' : 'NO') + '</td>' +
+                           yes_no(r.available) +
                        '</tr>';
             });
             $table.append(
                 '<tr>' +
                     '<td colspan=3>' + name + '</td>' +
-                    '<td>' + (details.available ? 'YES' : 'NO') + '</td>' +
+                    yes_no(details.available) +
                 '</tr>' +
                 requirements.join("")
             );
