c6f43f29ecdbab857c182a0ea7f1646f2cc80eb9
Marat Fayzullin <luarvique@gmail.com>
2023-11-06 22:54:39 -0500

    Now saving section states.

--- htdocs/lib/UI.js
+++ htdocs/lib/UI.js
@@ -14,6 +14,14 @@ UI.nrThreshold = 0;
 UI.nrEnabled = false;
 UI.wheelSwap = false;
 
+// Foldable UI sections and their initial states
+UI.sections = {
+    'modes'   : true,
+    'controls': true,
+    'settings': false,
+    'display' : true
+};
+
 //
 // Local Storage Access
 //
@@ -46,23 +54,25 @@ UI.loadBool = function(key) {
 // Load UI settings from local storage.
 UI.loadSettings = function() {
     if (this.has('ui_theme'))     this.setTheme(this.loadStr('ui_theme'));
-    if (this.has('ui_frame'))     this.setFrame(this.loadBool('ui_frame'));
     if (this.has('ui_opacity'))   this.setOpacity(this.loadInt('ui_opacity'));
-    if (this.has('ui_wheel'))     this.setWheelSwap(this.loadBool('ui_wheel'));
+    if (this.has('ui_frame'))     this.toggleFrame(this.loadBool('ui_frame'));
+    if (this.has('ui_wheel'))     this.toggleWheelSwap(this.loadBool('ui_wheel'));
     if (this.has('volume'))       this.setVolume(this.loadInt('volume'));
     if (this.has('nr_threshold')) this.setNR(this.loadInt('nr_threshold'));
+    if (this.has('nr_enabled'))   this.toggleNR(this.loadBool('nr_enabled'));
 
     // Reapply mute
     if (this.has('volumeMuted')) {
         var x = this.loadInt('volumeMuted');
-        if ((x >= 0) != (this.volumeMuted >= 0)) this.toggleMute();
+        this.toggleMute(x>=0);
         this.volumeMuted = x;
     }
 
-    // Reapply NR
-    if (this.has('nr_enabled')) {
-        var x = this.loadBool('nr_enabled');
-        if (x != this.nrEnabled) this.toggleNR();
+    // Toggle UI sections
+    for (const [section, state] of Object.entries(this.sections)) {
+        var id = 'openwebrx-section-' + section;
+        var el = document.getElementById(id);
+        if (el) this.toggleSection(el, this.has(id)? this.loadBool(id) : state);
     }
 };
 
@@ -75,22 +85,24 @@ UI.setVolume = function(x) {
     if (this.volume != x) {
         this.volume = x;
         this.save('volume', x);
-        $("#openwebrx-panel-volume").val(x)
+        $('#openwebrx-panel-volume').val(x)
         if (audioEngine) audioEngine.setVolume(x / 100);
     }
 };
 
 // Toggle audio muting.
-UI.toggleMute = function() {
+UI.toggleMute = function(on) {
+    // If no argument given, toggle mute
+    var toggle = typeof(on)==='undefined';
     var $muteButton = $('.openwebrx-mute-button');
     var $volumePanel = $('#openwebrx-panel-volume');
 
-    if ($volumePanel.prop('disabled')) {
+    if ($volumePanel.prop('disabled') && (toggle || !on)) {
         $muteButton.removeClass('muted');
         $volumePanel.prop('disabled', false);
         this.setVolume(this.volumeMuted);
         this.volumeMuted = -1;
-    } else {
+    } else if (toggle || on) {
         $muteButton.addClass('muted');
         $volumePanel.prop('disabled', true);
         this.volumeMuted = this.volume;
@@ -116,9 +128,12 @@ UI.setNR = function(x) {
 };
 
 // Toggle noise reduction function.
-UI.toggleNR = function() {
+UI.toggleNR = function(on) {
     var $nrPanel = $('#openwebrx-panel-nr');
-    this.nrEnabled = !!$nrPanel.prop('disabled');
+
+    // If no argument given, toggle NR
+    this.nrEnabled = !!(typeof(on)==='undefined'? $nrPanel.prop('disabled') : on);
+
     this.save('nr_enabled', this.nrEnabled);
     $nrPanel.prop('disabled', !this.nrEnabled);
     this.updateNR();
@@ -127,10 +142,10 @@ UI.toggleNR = function() {
 // Send changed noise reduction parameters to the server.
 UI.updateNR = function() {
     ws.send(JSON.stringify({
-        "type": "connectionproperties",
-        "params": {
-            "nr_enabled": this.nrEnabled,
-            "nr_threshold": this.nrThreshold
+        'type': 'connectionproperties',
+        'params': {
+            'nr_enabled': this.nrEnabled,
+            'nr_threshold': this.nrThreshold
         }
     }));
 }
@@ -139,34 +154,60 @@ UI.updateNR = function() {
 // Look & Feel Controls
 //
 
+UI.toggleSection = function(el, on) {
+    // If no argument given, toggle section
+    var toggle = typeof(on) === 'undefined';
+
+    var next_el = el.nextElementSibling;
+    if (next_el) {
+        if ((next_el.style.display === 'none') && (toggle || on)) {
+            el.innerHTML = el.innerHTML.replace('\u25B4', '\u25BE');
+            next_el.style.display = 'block';
+            this.save(el.id, true);
+        } else if (toggle || !on) {
+            el.innerHTML = el.innerHTML.replace('\u25BE', '\u25B4');
+            next_el.style.display = 'none';
+            this.save(el.id, false);
+        }
+    }
+};
+
 // Show or hide frame around receiver and other panels.
-UI.setFrame = function(x) {
-    if (this.frame != x) {
-        this.frame = x;
-        this.save('ui_frame', x);
-        $('#openwebrx-frame-checkbox').attr('checked', x);
-        $('#openwebrx-panel-receiver').css('border', x? '2px solid':'');
-        $('#openwebrx-dialog-bookmark').css('border', x? '2px solid':'');
+UI.toggleFrame = function(on) {
+    // If no argument given, toggle frame
+    if (typeof(on) === 'undefined') on = !this.frame;
+
+    if (this.frame != on) {
+        this.frame = on;
+        this.save('ui_frame', on);
+        $('#openwebrx-frame-checkbox').attr('checked', on);
+        $('#openwebrx-panel-receiver').css('border', on? '2px solid':'');
+        $('#openwebrx-dialog-bookmark').css('border', on? '2px solid':'');
     }
 };
 
 // Get current mouse wheel function
-UI.getWheelSwap = function(x) {
+UI.getWheelSwap = function() {
     return this.wheelSwap;
 };
 
 // Set mouse wheel function (zooming when swapped)
-UI.setWheelSwap = function(x) {
-    if (this.wheelSwap != x) {
-        this.wheelSwap = x;
-        this.save('ui_wheel', x);
-        $('#openwebrx-wheel-checkbox').attr('checked', x);
+UI.toggleWheelSwap = function(on) {
+    // If no argument given, toggle wheel swap
+    if (typeof(on) === 'undefined') on = !this.wheelSwap;
+
+    if (this.wheelSwap != on) {
+        this.wheelSwap = on;
+        this.save('ui_wheel', on);
+        $('#openwebrx-wheel-checkbox').attr('checked', on);
     }
 };
 
 // Set user interface opacity in 10..100% range.
 UI.setOpacity = function(x) {
+    // Limit opacity to 10..100% range
     x = x<10? 10 : x>100? 100 : x;
+
     if (this.opacity != x) {
         this.opacity = x;
         this.save('ui_opacity', x);
