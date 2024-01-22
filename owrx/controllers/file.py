fa913be3064700d0e0679d230ad883f689a4c348
Marat Fayzullin <luarvique@gmail.com>
2024-01-21 19:19:48 -0500

    Added ability to delete files from storage, when authorized.

--- owrx/controllers/file.py
+++ owrx/controllers/file.py
@@ -1,16 +1,41 @@
+from owrx.controllers.admin import Authentication, AuthorizationMixin
 from owrx.controllers.template import WebpageController
 from owrx.controllers.assets import AssetsController
 from owrx.storage import Storage
 
+import json
 import re
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class FileController(AssetsController):
     def getFilePath(self, file):
         return Storage.getFilePath(file)
 
 
+class FileDeleteController(AuthorizationMixin, WebpageController):
+    def delete(self):
+        try:
+            data = json.loads(self.get_body().decode("utf-8"))
+            file = data["name"].strip() if "name" in data else ""
+            if len(file) > 0 and re.match(Storage.getNamePattern(), file):
+                file = Storage.getFilePath(file)
+                logger.info("Deleting '{0}'.".format(file))
+                os.remove(file)
+            self.send_response("{}", content_type="application/json", code=200)
+        except Exception as e:
+            logger.debug("delete(): " + str(e))
+            self.send_response("{}", content_type="application/json", code=400)
+
+
 class FilesController(WebpageController):
     def template_variables(self):
+        # We need to know if the user is authorized to delete files
+        user  = Authentication().getUser(self.request)
+        admin = user is not None and user.is_enabled() and not user.must_change_password
         isimg = re.compile(r'.*\.(png|bmp|gif|jpg)$')
         issnd = re.compile(r'.*\.(mp3|wav)$')
         files = Storage.getSharedInstance().getStoredFiles()
@@ -27,12 +52,19 @@ class FilesController(WebpageController):
                 shot = "static/gfx/audio-file.png"
             else:
                 shot = "static/gfx/text-file.png"
+            # Admin user gets to delete files
+            if admin:
+                buttons = ('<button type="button" ' +
+                    'class="btn btn-sm btn-danger file-delete" ' +
+                    'value="' + files[i] + '">delete</button>')
+            else:
+                buttons = ""
             # Print out individual tiles
             rows += ('<td class="file-tile">' +
                 ('<a href="/files/%s" download="%s">' % (files[i], files[i])) +
                 ('<img src="%s" download="%s">' % (shot, files[i])) +
                 ('<p class="file-title">%s</p>' % files[i]) +
-                '</a></td>\n')
+                ('</a>%s</td>\n' % buttons))
             # Finish a row
             if i % 3 == 2:
                 rows += '</tr>\n'
