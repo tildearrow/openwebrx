6a45d4657d452513c6a4b5acd9dde2af8538cead
Marat Fayzullin <luarvique@gmail.com>
2024-02-03 23:10:00 -0500

    Functionality now works.

--- htdocs/lib/settings/Profiles.js
+++ htdocs/lib/settings/Profiles.js
@@ -1,14 +1,14 @@
 $.fn.profiles = function() {
     this.each(function() {
         $(this).on('click', '.move-down', function(e) {
-            $.ajax(document.URL.replace(/\/profile\//, '/moveprofiledown/')).done(function() {
+            $.ajax(document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofiledown/$2')).done(function() {
                 document.location.reload();
             });
             return false;
         });
 
         $(this).on('click', '.move-up', function(e) {
-            $.ajax(document.URL.replace(/\/profile\//, '/moveprofileup/')).done(function() {
+            $.ajax(document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofileup/$2')).done(function() {
                 document.location.reload();
             });
             return false;
