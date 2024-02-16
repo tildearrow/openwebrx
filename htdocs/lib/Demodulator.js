ff771f607f8f2c9a6b4dadd3aadb44b7120c5674
Marat Fayzullin <luarvique@gmail.com>
2024-02-16 00:25:12 -0500

    Fixing check for the fake caret indicator.

--- htdocs/lib/Demodulator.js
+++ htdocs/lib/Demodulator.js
@@ -51,7 +51,7 @@ Envelope.prototype.draw = function(visible_range){
 
     var from = center_freq + this.demodulator.offset_frequency;
     var to = center_freq + this.demodulator.offset_frequency;
-    var fake_indicator = !this.demodulator.low_cut || !this.demodulator.high_cut;
+    var fake_indicator = this.demodulator.low_cut==null || this.demodulator.high_cut==null;
     if (fake_indicator) {
         // fake values just so that the tuning indicator shows up
         var fixedBw = 100000
