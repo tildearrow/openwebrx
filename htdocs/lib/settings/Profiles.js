029a3109cc0d4aa5d11a24876c4c8b0f3979a5a9
Marat Fayzullin <luarvique@gmail.com>
2024-03-24 15:19:57 -0400

    Making sure moving profile is not stored in browser history.

--- htdocs/lib/settings/Profiles.js
+++ htdocs/lib/settings/Profiles.js
@@ -1,12 +1,12 @@
 $.fn.profiles = function() {
     this.each(function() {
         $(this).on('click', '.move-down', function(e) {
-            window.location = document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofiledown/$2');
+            location.replace(document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofiledown/$2'));
             return false;
         });
 
         $(this).on('click', '.move-up', function(e) {
-            window.location = document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofileup/$2');
+            location.replace(document.URL.replace(/(\/sdr\/[^\/]+)\/profile\/([^\/]+)$/, '$1/moveprofileup/$2'));
             return false;
         });
     });
