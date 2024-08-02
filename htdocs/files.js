1c922e3c99d7af07cefe6b35b933ce2f48602607
chenv <413137366@qq.com>
2024-08-03 06:55:05 +0800

    Update files.js (#56)
    Compatible with reverse proxy

--- htdocs/files.js
+++ htdocs/files.js
@@ -1,7 +1,7 @@
 $.fn.fileTile = function() {
     this.each(function() {
         $(this).on('click', '.file-delete', function(e) {
-            $.ajax("/files/delete", {
+            $.ajax("files/delete", {
                 data: JSON.stringify({ name: this.value }),
                 contentType: 'application/json',
                 method: 'POST'
