309274a6af8f228715c9948078472302a6c9daee
jeskko <jarkko@vaaraniemi.net>
2024-10-21 03:40:44 +0300

    Add bias-tee support for rtl_tcp source (#61)

--- owrx/source/rtl_tcp.py
+++ owrx/source/rtl_tcp.py
@@ -1,7 +1,7 @@
 from owrx.source.connector import ConnectorSource, ConnectorDeviceDescription
 from owrx.command import Flag, Option, Argument
 from owrx.form.input import Input
-from owrx.form.input.device import RemoteInput, DirectSamplingInput
+from owrx.form.input.device import RemoteInput, DirectSamplingInput, BiasTeeInput
 from owrx.form.input.validator import Range
 from typing import List
 
@@ -27,16 +27,16 @@ class RtlTcpDeviceDescription(ConnectorDeviceDescription):
         return "RTL-SDR device (via rtl_tcp)"
 
     def getInputs(self) -> List[Input]:
-        return super().getInputs() + [RemoteInput(), DirectSamplingInput()]
+        return super().getInputs() + [RemoteInput(), DirectSamplingInput(), BiasTeeInput()]
 
     def getDeviceMandatoryKeys(self):
         return super().getDeviceMandatoryKeys() + ["remote"]
 
     def getDeviceOptionalKeys(self):
-        return super().getDeviceOptionalKeys() + ["direct_sampling"]
+        return super().getDeviceOptionalKeys() + ["direct_sampling", "bias_tee"]
 
     def getProfileOptionalKeys(self):
-        return super().getProfileOptionalKeys() + ["direct_sampling"]
+        return super().getProfileOptionalKeys() + ["direct_sampling", "bias_tee"]
 
     def getSampleRateRanges(self) -> List[Range]:
         return [Range(250000, 3200000)]
