c7573b897fef004e4d75e273f04ee154abaff3c2
Marat Fayzullin <luarvique@gmail.com>
2025-01-20 09:38:14 -0500

    Incrementing version.

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.75"
+_versionstring = "1.2.76"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
